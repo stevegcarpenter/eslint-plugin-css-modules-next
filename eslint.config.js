30d97074ebe7365a8e60c8b167a954490b4e5a16
Steven Carpenter <stevegcarpenter@gmail.com>
2026-03-06 10:00:27 -0600

    Be permissive of files not captured in the linting

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,9 @@ export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ['eslint.config.js'],
+        },
       },
     },
   }
